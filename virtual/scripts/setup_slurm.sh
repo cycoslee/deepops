9608723e864066e4bf036e9fdc254de5e8ca07ea
Adam DeConinck <adeconinck@nvidia.com>
2019-03-07 16:49:54 -0800

    Virtual slurm cluster should include nfs homedirs

--- virtual/scripts/setup_slurm.sh
+++ virtual/scripts/setup_slurm.sh
@@ -10,9 +10,17 @@ ROOT_DIR="${SCRIPT_DIR}/../.."
 # Configure slurm in virtual cluster 
 #####################################
 
-# Move working directory to root of DeepOps repo
+# Move working directory to root of DeepOps repo 
 cd "${ROOT_DIR}" || exit 1
 
+# Configure NFS for shared home directories
+ansible-playbook \
+	-i "${VIRT_DIR}/config/inventory" \
+	-l slurm-cluster \
+	-e "@${VIRT_DIR}/vars_files/nfs_homedirs.yml" \
+	"${ROOT_DIR}/playbooks/nfs.yml"
+
+# Configure Slurm cluster
 ansible-playbook \
 	-i "${VIRT_DIR}/config/inventory" \
 	-l slurm-cluster \
